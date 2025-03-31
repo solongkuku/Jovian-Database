3846f1cdce0a28c0b43d44503c4d6bd745d497e8
avin-mendiratta <avin.mendiratta.mat24@itbhu.ac.in>
2025-03-31 14:53:38 +0530

    Delete database connection directory

--- database connection/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module youtubevids.databaseconnection {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    opens youtubevids.databaseconnection to javafx.fxml;
-    exports youtubevids.databaseconnection;
-}
